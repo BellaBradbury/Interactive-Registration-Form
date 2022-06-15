345092838e2d7b126fb63220c59be786f3658a2b
BellaBradbury <bellabradbury@yahoo.com>
2022-06-15 09:56:55 -0400

    color drop down code
    STEP 5
    
    NO ERRORS

--- js/script.js
+++ js/script.js
@@ -14,3 +14,22 @@ jobRoles.addEventListener('change', (event) => {
     otherRole.style.display = 'none';
   }
 });
+
+// ALLOWS USER TO CHOOSE A COLOR OPTION BASED ON THEIR DESIGN CHOICE
+const colorList = document.getElementById('color');
+const colors = document.querySelectorAll('#color option');
+const designList = document.getElementById('design');
+
+colorList.disabled = true;
+
+designList.addEventListener('change', (event) => {
+  colorList.disabled = false;
+
+  colors.forEach( color => {
+    if (event.target.value === color.getAttribute('data-theme')) {
+      color.style.display = 'block';
+    } else {
+      color.style.display = 'none';
+    }
+  });
+});
