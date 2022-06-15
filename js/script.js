20ea43377322a3c66a0c9758ad33d06bd42d8a71
BellaBradbury <bellabradbury@yahoo.com>
2022-06-15 09:17:44 -0400

    added 'other' job code
    STEP 4
    
    NO ERRORS

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 // FOCUS ON FIRST FIELD BY DEFAULT
-const nameInput = document.getElementById('name').focus();
+const defaultFocus = document.getElementById('name').focus();
+
+// DISPLAYS 'OTHER' JOB ROLE INPUT
+const jobRoles = document.getElementById('title');
+const otherRole = document.getElementById('other-job-role');
+
+otherRole.style.display = 'none';
+
+jobRoles.addEventListener('change', (event) => {
+  if (event.target.value === 'other') {
+    otherRole.style.display = 'block';
+  } else {
+    otherRole.style.display = 'none';
+  }
+});
